1f0c05e021a73be054c852f46f240cb3dd84a6b6
Marshall <gevent@CSSI2015-32.local>
2015-07-24 12:24:54 -0400

    Refactored remark code to make it more concise.

--- remark_datastore.py
+++ remark_datastore.py
@@ -31,13 +31,13 @@ def ReadRemarks(user_id):
   # Query the datastore for remarks. Only get remarks posted since the last time
   # this user checked (Remark.timestamp >= start_time). Order them by when they
   # were posted. Return the results as a list of tuples with (user, text, color).
-  remarks = []
-  query = Remark.query(Remark.timestamp >= start_time).order(Remark.timestamp)
-  for remark in query.fetch():
-    # TODO(cssi-cam-2015) Randomize the color so that each remark is different.
-    remarks.append((remark.user, remark.text, 'black'))
 
-  return remarks
+  # TODO(cssi-cam-2015) Randomize the color so that each remark is different.
+  return [
+      (remark.user, remark.text, 'black')
+      for remark
+      in Remark.query(
+          Remark.timestamp >= start_time).order(Remark.timestamp).fetch()]
 
 
 def PostRemark(user, text):
